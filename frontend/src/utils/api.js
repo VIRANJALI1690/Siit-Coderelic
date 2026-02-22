f3237320e120e6dd138653661542d820d7663bbd
Viranjali Game <gameviranjali1690@gmail.com>
2026-02-22 10:16:12 +0530

    Update API base URL to production endpoint

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://siit-backend-hvs7.onrender.com/',
 });
 
 // Add a request interceptor to include the token
