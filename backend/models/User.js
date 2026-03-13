a1730ba76dcdbec6a33d1e81a46e4fed175000bb
VIRANJALI1690 <gameviranjali1690@gmail.com>
2026-03-13 11:47:30 +0530

    Updated User model password middleware

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,12 +18,13 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save', async function () {
+userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        return;
+        return next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
